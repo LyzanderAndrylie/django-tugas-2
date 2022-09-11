a55125c10baca9d4b2ce86930a3fa116c19dd518
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2022-09-11 19:41:53 +0700

    Membuat fungsi pada views.py
    Membuat sebuah fungsi pada views.py yang dapat melakukan pengambilan
    data dari model dan dikembalikan ke dalam sebuah HTML.

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_item': data_katalog,
+        'nama': 'Lyzander Marciano Andrylie',
+        'id': '2106750755',
+    }
+
+    return render(request, "katalog.html", context)
