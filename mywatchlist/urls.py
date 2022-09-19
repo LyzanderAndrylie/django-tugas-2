d2ba2ff689d29c798b7311ba7902b088f945b025
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2022-09-19 20:20:00 +0700

    Membuat routing

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from mywatchlist.views import *
 
 app_name = 'mywatchlist'
 
 urlpatterns = [
+    path('html/', show_mywatchlist, name='show_wishlist'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
 ]
