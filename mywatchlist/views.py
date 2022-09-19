011cd75baf86489969786f70318ed12d7e21f003
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2022-09-19 20:13:07 +0700

    Membuat fungsi pada views.py untuk aplikasi mywatchlist

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from mywatchlist.models import MyWatchList
+
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
+def show_mywatchlist(request):
+    data_mywatchlist = MyWatchList.objects.all()
+    context = {
+        'mywatchlist': data_mywatchlist,
+        'nama': 'Lyzander Marciano Andrylie',
+        'id': '2106750755'
+    }
+
+    return render(request, "mywatchlist.html", context)
+
+def show_xml(request):
+    data = MyWatchList.objects.all()
+
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = MyWatchList.objects.all()
+
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
