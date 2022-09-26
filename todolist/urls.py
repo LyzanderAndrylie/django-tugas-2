b56a9f0b56c04b98ae2579d78bafbd12f3648a00
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2022-09-26 15:17:27 +0700

    Membuat form registrasi untuk aplikasi todolist

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from todolist.views import register
 
 app_name = 'todolist'
 
 urlpatterns = [
-    
+    path('register/', register, name='register'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 # Create your views here.
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('todolist:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
