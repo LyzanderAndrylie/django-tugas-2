dc57de0c8e0b6ccc9cc30e25918415357f6462c0
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2022-09-26 15:42:18 +0700

    Membuat form login untuk aplikasi todolist

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from todolist.views import register
 
+from todolist.views import login_user
+
 app_name = 'todolist'
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
+from django.contrib.auth import authenticate, login
+
 # Create your views here.
 def register(request):
     form = UserCreationForm()
@@ -17,3 +19,16 @@ def register(request):
     
     context = {'form':form}
     return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('todolist:show_todolist')
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
